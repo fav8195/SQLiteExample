37611c9e5648e562a050b813f30209b257d5237e
Work <filinkov@u-plast.by>
2022-05-17 09:24:10 +0300

    Initial commit

---
+++ app/src/main/java/by/fav8195/sqliteexample/MainActivity.kt
@@ -0,0 +1,11 @@
+package by.fav8195.sqliteexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
